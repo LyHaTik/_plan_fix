abcca3ef76906bea4e23769ff9836c19316da7dd
LyHaTik <96049450+LyHaTik@users.noreply.github.com>
2024-05-22 21:52:07 +0700

    Update telegram-web-app.js

--- telegram-web-app.js
+++ telegram-web-app.js
@@ -2,25 +2,41 @@ let tg = window.Telegram.WebApp;
 let create_tr = document.getElementById("create_tr");
 
 create_tr.addEventListener("click", () => {
+	let arendator = document.getElementById("id_arendator").value;
 	let date_start = document.getElementById("id_date_start").value;
 	let date_finish = document.getElementById("id_date_finish").value;
-	let time_tr = document.getElementById("id_time_tr").value;
-	let location = document.getElementById("id_location").value;
-	let hidden_survey = document.getElementById("id_hidden_survey").checked;
-	let type_tr = document.getElementById("id_type_tr").value;
-	let count_gamer = document.getElementById("id_count_gamer").value;
+	let adres = document.getElementById("id_adres").value;
+	let ts = document.getElementById("id_ts").value;
+	let period = document.getElementById("id_period").value;
+	let prepay = document.getElementById("id_prepay").value;
+	let currency = document.getElementById("id_currency").value;
+	let tarif = document.getElementById("id_tarif").value;
+	let deposite = document.getElementById("id_deposite").value;
+	let count = document.getElementById("id_count").value;
+	let hz = document.getElementById("id_hz").value;
+	let delivery = document.getElementById("id_delivery").checked;
+	let location_start = document.getElementById("id_location_start").value;
+	let undelivery = document.getElementById("id_undelivery").checked;
+	let location_finish = document.getElementById("id_location_finish").value;
 	let comment = document.getElementById("id_comment").value;
-	let level = document.getElementById("id_level").value;
 	let data = {
+		arendator: arendator,
 		date_start: date_start,
 		date_finish: date_finish,
-		time_tr: time_tr,
-		location: location,
-		hidden_survey: hidden_survey,
-		type_tr: type_tr,
-		count_gamer: count_gamer,
-		comment: comment,
-		level: level
+		adres: adres,
+		ts: ts,
+		period: period,
+		prepay: prepay,
+		currency: currency,
+		tarif: tarif,
+		deposite: deposite,
+		count: count,
+		hz: hz,
+		delivery: delivery,
+		location_start: location_start,
+		undelivery: undelivery,
+		location_finish: location_finish,
+		comment: comment
 		}
 		tg.sendData(JSON.stringify(data));
 		tg.close()
