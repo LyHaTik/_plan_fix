270f573e0df84a6600f560431c9ce974d6f4af13
LyHaTik <96049450+LyHaTik@users.noreply.github.com>
2024-05-22 22:27:24 +0700

    Update telegram-web-app.js

--- telegram-web-app.js
+++ telegram-web-app.js
@@ -1,5 +1,5 @@
 let tg = window.Telegram.WebApp;
-let booking = document.getElementById("booking");
+let booking = document.getElementById("id_booking");
 
 booking.addEventListener("click", () => {
 	let arendator = document.getElementById("id_arendator").value;
