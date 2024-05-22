02a7c8a55ccf42c99e3ed60a30d188fe443e94f4
LyHaTik <96049450+LyHaTik@users.noreply.github.com>
2024-05-22 14:20:11 +0700

    Add files via upload

---
+++ telegram-web-app.js
@@ -0,0 +1,34 @@
+let tg = window.Telegram.WebApp;
+let create_tr = document.getElementById("create_tr");
+
+create_tr.addEventListener("click", () => {
+	let date_tr = document.getElementById("id_date_tr").value;
+	let time_tr = document.getElementById("id_time_tr").value;
+	let location = document.getElementById("id_location").value;
+	let hidden_survey = document.getElementById("id_hidden_survey").checked;
+	let type_tr = document.getElementById("id_type_tr").value;
+	let count_gamer = document.getElementById("id_count_gamer").value;
+	let comment = document.getElementById("id_comment").value;
+	let level = document.getElementById("id_level").value;
+	let data = {
+		date_tr: date_tr,
+		time_tr: time_tr,
+		location: location,
+		hidden_survey: hidden_survey,
+		type_tr: type_tr,
+		count_gamer: count_gamer,
+		comment: comment,
+		level: level
+		}
+		tg.sendData(JSON.stringify(data));
+		tg.close()
+	});
+
+var c = document.querySelector('#id_hidden_survey');
+c.onclick = function() {
+ if (c.checked) {
+  alert( '🥷 Скрытая тренировка: опрос тренировки в личку' );
+ } else {
+  alert( 'Открытая тренировка: опрос тренировки в личку и группу' );
+ }
+}
