5942d74925e08b0a1868e37df142da69dff2cd91
LyHaTik <96049450+LyHaTik@users.noreply.github.com>
2024-05-23 12:08:32 +0700

    Update telegram-web-app.js

--- telegram-web-app.js
+++ telegram-web-app.js
@@ -3,11 +3,6 @@ let booking = document.getElementById("booking");
 
 booking.addEventListener("click", () => {
 	let arendator = document.getElementById("id_arendator").value;
-	let date_start = document.getElementById("id_date_start").value;
-	let date_finish = document.getElementById("id_date_finish").value;
-	let adres = document.getElementById("id_adres").value;
-	let ts = document.getElementById("id_ts").value;
-	let period = document.getElementById("id_period").value;
 	let data = {
 		arendator: arendator,
 		date_start: date_start,
