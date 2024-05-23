2cae934b49d90cd91a20be5d20b49df7d8f78a45
LyHaTik <96049450+LyHaTik@users.noreply.github.com>
2024-05-23 11:42:44 +0700

    Update telegram-web-app.js

--- telegram-web-app.js
+++ telegram-web-app.js
@@ -3,6 +3,22 @@ let booking = document.getElementById("booking");
 
 booking.addEventListener("click", () => {
 	let arendator = document.getElementById("id_arendator").value;
+	let date_start = document.getElementById("id_date_start").value;
+	let date_finish = document.getElementById("id_date_finish").value;
+	let adres = document.getElementById("id_adres").value;
+	let ts = document.getElementById("id_ts").value;
+	let period = document.getElementById("id_period").value;
+	let prepay = document.getElementById("id_prepay").value;
+	let currency = document.getElementById("id_currency").value;
+	let tarif = document.getElementById("id_tarif").value;
+	let deposite = document.getElementById("id_deposite").value;
+	let count = document.getElementById("id_count").value;
+	let hz = document.getElementById("id_hz").value;
+	let delivery = document.getElementById("id_delivery").checked;
+	let location_start = document.getElementById("id_location_start").value;
+	let undelivery = document.getElementById("id_undelivery").checked;
+	let location_finish = document.getElementById("id_location_finish").value;
+	let comment = document.getElementById("id_comment").value;
 	let data = {
 		arendator: arendator,
 		date_start: date_start,
